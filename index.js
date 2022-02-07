a572a7b35fbd72975fef87b5ec08c968534d8360
Batoul Alkarim <batoulalkarim1@gmail.com>
2022-02-07 10:40:35 -0500

    commit tree 86ccfebc17f1c0bb59556e32d5f95d946e36b3bc

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-function addingEventListener() {
-    const input = document.getElementById('input');
 
-function clickAlert() {
-  alert('I was clicked!');
-}
-
-input.addEventListener('click', clickAlert);
-
+function addingEventListener(){
+  const input = document.getElementById('input');
+  input.addEventListener('click', function(){
+    alert('I was clicked!');
+  })
 }
